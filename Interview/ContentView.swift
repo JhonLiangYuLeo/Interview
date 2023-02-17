2011adc44b1958e862082666e8438f1efa673ddb
jhonliang <jhonliang@tencent.com>
2023-02-17 17:16:59 +0800

    Initial Commit

---
+++ Interview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Interview
+//
+//  Created by 梁宇峰 on 2023/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
